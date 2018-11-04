13f3ecedf4f61c4e97130897436cd10b45a45742
Jarret Nachtigal <jarretn1224@gmail.com>
2018-11-03 20:51:44 -0400

    Update TestCreator.java

---
+++ TestCreator.java
@@ -0,0 +1,27 @@
+public class TestCreator{
+  public static void main(String[] args){
+    
+    boolean continue = true;
+    while (continue ==  true){
+      System.out.print("please enter a file type");
+      
+      System.out.print("please enter a test scheme");    
+    } 
+  } 
+  
+  public void giveIntroduction(){
+    System.out.println("Welcome to the test creator. Press q to quit");
+  }
+  
+  public File getFile(){
+    
+  }
+  
+  public TestScheme getScheme(){
+    
+  }
+  
+  // public Test createTest(FormatReader format, ){
+  // 
+  // }
+}
